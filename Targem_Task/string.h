322c303f7908d99f4a55948a980c9cf2665fdab8
Anrokyr <78145373+ShiroNoby@users.noreply.github.com>
2022-05-23 16:39:02 +0500

    Work version

--- Targem_Task/string.h
+++ Targem_Task/string.h
@@ -10,68 +10,68 @@ namespace string
     //Custom String
     class CString
     {
-	public:
+    public:
 
         ///ctor/dtor
                             CString() :m_length(0), m_data(nullptr){}
                             ~CString();
 
-		explicit			CString( const char* );
+	explicit	    CString( const char* );
                             CString( const CString& );
                             CString( CString&& ) noexcept;
 
-		CString&            operator=(CString&);
-		CString&            operator=(CString&&) noexcept;
+	CString&            operator=(CString&);
+	CString&            operator=(CString&&) noexcept;
 
 
     private:
 
-		//
-		explicit			CString(uint32_t);
+	//
+	explicit	    CString(uint32_t);
 
-		//new
-		void				Allocate( uint32_t );
+	//new
+	void		    Allocate( uint32_t );
 
-		//m_data[] = data[]
-		void                Access( const char* );
+	//m_data[] = data[]
+	void                Access( const char* );
 
 	public:
 	
         //operators whithouth range check
-        char&				operator[]( uint32_t );
-		const char&			operator[]( uint32_t ) const;
+        char&		    operator[]( uint32_t );
+	const char&         operator[]( uint32_t ) const;
 
-		//operators whith range chek
-		char&               at(uint32_t);
-		const char&         at(uint32_t) const;
+	//operators whith range chek
+	char&               at(uint32_t);
+	const char&         at(uint32_t) const;
 
-		bool			    operator<( const CString& ) const;
-		bool			    operator>( const CString& ) const;
-		bool                operator==( const CString& ) const;
+	bool		    operator<( const CString& ) const;
+	bool		    operator>( const CString& ) const;
+	bool                operator==( const CString& ) const;
 		
-		//concatination
-		friend CString		operator+( const CString&, const CString&);
-		friend CString		operator+( const CString& ,const char* );
-		friend CString		operator+( const char* , const CString& );
+	//concatination
+	friend CString	    operator+( const CString&, const CString&);
+	friend CString	    operator+( const CString& ,const char* );
+	friend CString	    operator+( const char* , const CString& );
 				
-		//gets
-		const char*			c_str() const;
-		uint32_t            length() const;
-		bool                empty() const;
+	//gets
+	const char*	    c_str() const;
+	uint32_t            length() const;
+	bool                empty() const;
 
-		//swap str1 <-> str2
-		void				swap( CString& str );
+	//swap str1 <-> str2
+	void		    swap( CString& str );
 
 	private:
 
-		uint32_t			m_length;
-		char*				m_data;
+	uint32_t	    m_length;
+	char*		    m_data;
 	};
 
 	//concatination
-	CString					operator+(const CString&, const CString&);
-	CString					operator+(const CString&, const char*);
-	CString					operator+(const char*, const CString&);
+	CString		    operator+(const CString&, const CString&);
+	CString		    operator+(const CString&, const char*);
+	CString		    operator+(const char*, const CString&);
 } // namespace string
 
 #endif // __STRING_H_
